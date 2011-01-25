5d4d859eae7c51c663fa8256cb87b99310c92f66
Nathan Middleton <nathan.middleton@gmail.com>
2011-01-24 19:10:23 -0700

    Fixed path to normalize and expand variables in path.  Allows use of tilde and environment variables in path to ipc sock file.

--- i3ipc.py
+++ i3ipc.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python
 
 from struct import pack, unpack
+from os import path
 import socket, json
 
-I3_IPCFILE = '~/i3/ipc.sock'
-I3_IPC_MAGIC = 'i3-ipc'
-I3_CHUNK_SIZE = 1024
+I3_IPCFILE = '~/i3/ipc.sock'            # default location of i3 ipc socket file
+I3_IPC_MAGIC = 'i3-ipc'                 # token used to identify i3 messages
+I3_CHUNK_SIZE = 1024                    # default receive size
 
 I3_IPC_MESSAGE_TYPE_COMMAND = 0
 I3_IPC_MESSAGE_TYPE_GET_WORKSPACES = 1
@@ -17,6 +18,11 @@ I3_IPC_REPLY_TYPE_WORKSPACES = 1
 I3_IPC_REPLY_TYPE_SUBSCRIBE = 2
 I3_IPC_REPLY_TYPE_OUTPUTS = 3
 
+__event_handlers = {}                   # subscribed event handlers
+
+
+def get_ipcfile_path(path):
+    return os.path.expanduser(os.path.expanvars(os.path.normpath(path)))
 
 def pack_message(*args):
     package = []
@@ -40,6 +46,7 @@ def unpack_response(data):
     return response
 
 def send(mtype, payload='', ipcfile=I3_IPCFILE):
+    ipcfile = path.expanduser(path.expandvars(path.normpath(ipcfile)))
     s = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
     message = pack_message(I3_IPC_MAGIC, len(payload), mtype, payload)
     s.connect(ipcfile)
@@ -49,7 +56,7 @@ def send(mtype, payload='', ipcfile=I3_IPCFILE):
     data = s.recv(I3_CHUNK_SIZE)
     while len(data) < expected_length(data):
         data = '%s%s' % (data, s.recv(I3_CHUNK_SIZE),)
-    s.close()
+    #s.close()
     return unpack_response(data)
 
 def send_command(payload, ipcfile=I3_IPCFILE):
@@ -60,3 +67,16 @@ def get_workspaces(ipcfile=I3_IPCFILE):
 
 def get_outputs(ipcfile=I3_IPCFILE):
     return send(I3_IPC_MESSAGE_TYPE_GET_OUTPUTS, ipcfile=ipcfile)
+
+def subscribe(event, handler, icpfile=I3_IPCFILE):
+    __event_handlers[event] = handler
+    pass
+
+
+"""
+Called when file is directly run.
+Useful only for testing as this module will not have any immediate use.
+"""
+if __name__ == '__main__':
+    ipcfile = '~/.config/i3/ipc.sock'
+    
