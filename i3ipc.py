614f905fee4072404061d2312173064a5ebe8117
Nathan Middleton <nathan.middleton@gmail.com>
2011-01-25 12:03:23 -0700

    Complete rewrite of previous code. * Moved all module functions into I3Socket class.
    Notes:
    I3Socket represents a line of communication with the i3 wm. Internally it
    maintains a socket that never closes until its close method is called. Each
    request from the socket will get immediate response through the calling
    functions.
    
    Todo:
    Add threading support for reading the socket on a loop for event subscription
    support.

--- i3ipc.py
+++ i3ipc.py
@@ -1,8 +1,8 @@
-#!/usr/bin/env python
-
+from socket import socket, AF_UNIX
+from os.path import expanduser, expandvars, normpath
 from struct import pack, unpack
-from os import path
-import socket, json
+import json
+
 
 I3_IPCFILE = '~/i3/ipc.sock'            # default location of i3 ipc socket file
 I3_IPC_MAGIC = 'i3-ipc'                 # token used to identify i3 messages
@@ -18,65 +18,75 @@ I3_IPC_REPLY_TYPE_WORKSPACES = 1
 I3_IPC_REPLY_TYPE_SUBSCRIBE = 2
 I3_IPC_REPLY_TYPE_OUTPUTS = 3
 
-__event_handlers = {}                   # subscribed event handlers
 
+class I3Socket:
+    def __init__(self, ipcfile=I3_IPCFILE):
+        self.__ipcfile = expanduser(expandvars(normpath(ipcfile)))
+        self.__socket = socket(AF_UNIX)
+        self.__socket.connect(self.__ipcfile)
 
-def get_ipcfile_path(path):
-    return os.path.expanduser(os.path.expanvars(os.path.normpath(path)))
+    def __pack_message(self, message_type, payload):
+        package = [I3_IPC_MAGIC]
+        package.append(pack('l', len(payload)))
+        package.append(pack('l', message_type))
+        package.append(payload)
+        return ''.join(package)
 
-def pack_message(*args):
-    package = []
-    for arg in args:
-        arg_type = type(arg)
-        if arg_type == str: package.append(arg)
-        elif arg_type == int: package.append(pack('l', arg))
-    return ''.join(package)
+    def __unpack_response(self, data):
+        response =  {
+            'magic': data[:6],
+            'total_length': len(data),
+            'length': unpack('l', data[6:10])[0],
+            'type': unpack('l', data[10:14])[0],
+            'payload_length': len(data[14:]),
+            'payload': json.loads(data[14:]),
+        }
+        if response['magic'] != I3_IPC_MAGIC:
+            raise KeyError("I3_IPC_MAGIC not present.")
+        self.__last_response = response
+        return response
+        
+    def close(self):
+        """ Close the socket. """
+        self.__socket.close()
 
-def unpack_response(data):
-    response =  {
-        'magic': data[:6],
-        'total_length': len(data),
-        'length': unpack('l', data[6:10])[0],
-        'type': unpack('l', data[10:14])[0],
-        'payload_length': len(data[14:]),
-        'payload': json.loads(data[14:]),
-    }
-    if response['magic'] != I3_IPC_MAGIC:
-        raise ValueError()
-    return response
-
-def send(mtype, payload='', ipcfile=I3_IPCFILE):
-    ipcfile = path.expanduser(path.expandvars(path.normpath(ipcfile)))
-    s = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
-    message = pack_message(I3_IPC_MAGIC, len(payload), mtype, payload)
-    s.connect(ipcfile)
-    s.send(message)
-    def expected_length(data):
-        return int(unpack('l', data[6:10])[0]) + 14
-    data = s.recv(I3_CHUNK_SIZE)
-    while len(data) < expected_length(data):
-        data = '%s%s' % (data, s.recv(I3_CHUNK_SIZE),)
-    s.close()
-    return unpack_response(data)
-
-def send_command(payload, ipcfile=I3_IPCFILE):
-    return send(I3_IPC_MESSAGE_TYPE_COMMAND, payload, ipcfile)
-
-def get_workspaces(ipcfile=I3_IPCFILE):
-    return send(I3_IPC_MESSAGE_TYPE_GET_WORKSPACES, ipcfile=ipcfile)
-
-def get_outputs(ipcfile=I3_IPCFILE):
-    return send(I3_IPC_MESSAGE_TYPE_GET_OUTPUTS, ipcfile=ipcfile)
-
-def subscribe(event, handler, icpfile=I3_IPCFILE):
-    __event_handlers[event] = handler
-    pass
-
-
-"""
-Called when file is directly run.
-Useful only for testing as this module will not have any immediate use.
-"""
-if __name__ == '__main__':
-    ipcfile = '~/.config/i3/ipc.sock'
+    def send(self, mtype, payload=''):
+        """ Send a raw message to the window manager. """
+        message = self.__pack_message(mtype, payload)
+        self.__socket.send(message)
+        def expected_length(data):
+            return int(unpack('l', data[6:10])[0]) + 14
+        data = self.__socket.recv(I3_CHUNK_SIZE)
+        while len(data) < expected_length(data):
+            data = '%s%s' % (data, self.__socket.recv(I3_CHUNK_SIZE),)
+        return self.__unpack_response(data)
     
+    def send_command(self, payload):
+        """ Send a command to window manager. """
+        return self.send(I3_IPC_MESSAGE_TYPE_COMMAND, payload)
+
+    def get_workspaces(self, workspace=None, raw=False):
+        """ Return a list of workspaces. """
+        response = self.send(I3_IPC_MESSAGE_TYPE_GET_WORKSPACES)
+        return response if raw else response['payload']
+
+    def get_workspace(self, workspace):
+        """ Query workspaces by index (zero-based) or name (unicode string). """
+        t = type(workspace)
+        if t == int:
+            return response['payload'][workspace - 1]
+        elif t == str:
+            for ws in response['payload']:
+                if ws['name'] == ws:
+                    ws
+
+    def get_outputs(self, raw=False):
+        """ Returns a list of of available outpus. """
+        response = self.send(I3_IPC_MESSAGE_TYPE_GET_OUTPUTS)
+        return response if raw else response['payload']
+
+
+
+if __name__ == '__main__':
+    # test location for XDG_CONFIG_HOME
+    i3s = I3Socket('~/.config/i3/ipc.sock')
