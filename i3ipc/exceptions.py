3b543d7829f94fb09b96a44b6545359edecf2e5c
Nathan Middleton <nathan.middleton@gmail.com>
2011-01-28 03:20:47 -0700

    several changes to module structure. changes are as follows;   * Moved I3Socket back into main module and removed package;   * Added I3EventListener class;   * Added subscribe function as generator for event listeners. Call the     unsubscribe method to stop listening;   * integrated class-based socket model back into module file;   * moved pack/unpack functions into module as utility functions;   * And, probably other little things being missed.

--- i3ipc/exceptions.py
+++
@@ -1,7 +0,0 @@
-class MagicKeyError(Exception):
-    def __init__(self):
-        Exception.__init__(self, "Magic key not found in response. Response tainted!")
-
-class NotFoundError(Exception):
-    def __init__(self, msg):
-        Exception.__init__(self, msg)
