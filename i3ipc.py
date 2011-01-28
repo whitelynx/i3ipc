1563e37ef86d84d325930cfab32eb986bb4a434c
Nathan Middleton <nathan.middleton@gmail.com>
2011-01-28 04:52:07 -0700

    fixed blocking issue with I3Socket, moved exception checking from I3EventListener to I3Socket when trying to recieve data for non-blocking sockets.

--- i3ipc.py
+++ i3ipc.py
@@ -3,8 +3,7 @@ import json, threading, struct, socket, time
 
 I3_IPCFILE = '~/i3/ipc.sock'            # default location of i3 ipc socket file
 I3_IPC_MAGIC = 'i3-ipc'                 # token used to identify i3 messages
-I3_CHUNK_SIZE = 1024                    # default receive size
-I3_EVENT_DELAY = 1                      # event thread sleep times
+I3_CHUNK_SIZE =  4096                   # default receive size
 
 I3_IPC_MESSAGE_TYPE_COMMAND = 0
 I3_IPC_MESSAGE_TYPE_GET_WORKSPACES = 1
@@ -56,10 +55,11 @@ def subscribe(callback, event_list, ipcfile=I3_IPCFILE):
 
 
 class I3Socket:
-    def __init__(self, ipcfile=I3_IPCFILE, blocking=True):
+    def __init__(self, ipcfile=I3_IPCFILE, blocking=True, chunk_size=I3_CHUNK_SIZE):
+        self.__chunk_size = chunk_size
         self.__ipcfile = ipcfile
         self.__socket = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
-        self.__socket.setblocking(blocking)
+        if not blocking: self.__socket.setblocking(False)
         self.__socket.connect(expanduser(expandvars(normpath(self.__ipcfile))))
 
     def send(self, mtype, payload=''):
@@ -71,9 +71,26 @@ class I3Socket:
         By default a socket is using I3_IPCFILE. If an ipcfile argument is supplied
         the module will store the new position for future reference. So ipcfile must
         only be set once."""
-        self.__socket.send(pack(mtype, payload))
-        response = unpack(self.recieve())['payload']
-        return response
+        message = pack(mtype, payload)
+        self.__socket.sendall(message)
+        data = self.recieve()
+        response = unpack(data)
+        return response['payload']
+
+    def recieve(self):
+        """ Recieve data from the socket. """
+        try:
+            data = self.__socket.recv(self.__chunk_size)
+            expected_length = int(struct.unpack('l', data[6:10])[0]) + 14
+            while len(data) < expected_length:
+                data = '%s%s' % (data, self.__socket.recv(self.__chunk_size),)
+            return data
+        except:
+            return None
+
+    def close(self):
+        """ close this socket if open. """
+        self.__socket.close()
 
     def send_command(self, payload):
         """ Send a command to window manager. """
@@ -87,38 +104,28 @@ class I3Socket:
         """ Returns a list of of available outpus. """
         return self.send(I3_IPC_MESSAGE_TYPE_GET_OUTPUTS)
 
-    def close(self):
-        """ close this socket if open. """
-        self.__socket.close()
-
-    def recieve(self):
-        """ Recieve data from the socket. """
-        data = self.__socket.recv(I3_CHUNK_SIZE)
-        expected_length = int(struct.unpack('l', data[6:10])[0]) + 14
-        while len(data) < expected_length:
-            data = '%s%s' % (data, self.__socket.recv(I3_CHUNK_SIZE),)
-        return data
+    def subscribe(self, subscriptions):
+        """ Subscribe to an event over this socket. Used by I3EventListener. """
+        payload = json.dumps(subscriptions)
+        return self.send(I3_IPC_MESSAGE_TYPE_SUBSCRIBE, payload)
 
 
 class I3EventListener(threading.Thread):
     """ i3 callback event listening thread. """
     def __init__(self, callback, event_list, ipcfile=I3_IPCFILE):
         threading.Thread.__init__(self)
-        self.__socket = I3Socket(ipcfile, False)
         self.__callback = callback
-        subscription = json.dumps(event_list)
-        response = self.__socket.send(I3_IPC_MESSAGE_TYPE_SUBSCRIBE, subscription)
+        self.__socket = I3Socket(ipcfile, blocking=False)
+        self.__socket.subscribe(event_list)
         self.__subscribed = True
         self.start()
 
     def run(self):
         while self.__subscribed:
-            try:
-                data = self.__socket.recieve()
+            data = self.__socket.recieve()
+            if data:
                 response = unpack(data)['payload']
                 self.__callback(response)
-            except:
-                pass
 
     def unsubscribe(self):
         self.__socket.close()
@@ -132,4 +139,4 @@ if __name__ == '__main__':
 
     ipcfile = '~/.config/i3/ipc.sock'
     i3s = I3Socket(ipcfile)
-    i3e = subscribe(printit, 'workspace', ipcfile=ipcfile)
+    i3e = subscribe(printit, ['workspace'], ipcfile=ipcfile)
